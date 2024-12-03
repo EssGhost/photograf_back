2628f061822052aef7211a561c1b99a14cedc78e
EssGhost <yolotlperez8@gmail.com>
2024-12-03 03:29:33 -0600

    cambio de funcion create de cbu

--- src/courtesies_by_user/courtesies_by_user.module.ts
+++ src/courtesies_by_user/courtesies_by_user.module.ts
@@ -4,10 +4,11 @@ import { CourtesiesByUserController } from './courtesies_by_user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { courtesies_by_user } from './entities/courtesies_by_user.entity';
 import { UsersModule } from 'src/users/users.module';
+import { courtesies_by_group } from '../courtesies_by_group/entities/courtesies_by_group.entity';
 
 @Module({
   controllers: [CourtesiesByUserController],
   providers: [CourtesiesByUserService],
-  imports: [TypeOrmModule.forFeature([courtesies_by_user]), UsersModule],
+  imports: [TypeOrmModule.forFeature([courtesies_by_user, courtesies_by_group]), UsersModule],
 })
 export class CourtesiesByUserModule {}
--- src/courtesies_by_user/dto/create-courtesies_by_user.dto.ts
+++ src/courtesies_by_user/dto/create-courtesies_by_user.dto.ts
@@ -1,16 +1,17 @@
 import { Type } from "class-transformer";
 import { IsNumber, IsPositive } from "class-validator";
 import { courtesies_by_group } from "src/courtesies_by_group/entities/courtesies_by_group.entity";
-import { users } from "src/users/entities/user.entity";
+
 
 export class CreateCourtesiesByUserDto {
-    // @IsNumber()
-    // @IsPositive()
-    // @Type(() => users )
-    // user: users;
     
     @IsNumber()
     @IsPositive()
-    @Type(() => courtesies_by_group )
-    courtesy_by_group: courtesies_by_group;
+    //@Type(() => courtesies_by_group )
+    courtesy_by_group: number;
 }
+
+// @IsNumber()
+// @IsPositive()
+// @Type(() => users )
+// user: users;
